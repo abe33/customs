4b167e535373b7a8f7d5d2a73a9987e814cb5986
Savater Sebastien <savater.sebastien@gmail.com>
2012-07-06 02:23:24 +0200

    Logging rescues

--- lib/cancan-traffic/controller_additions.rb
+++ lib/cancan-traffic/controller_additions.rb
@@ -17,12 +17,19 @@ module CanCanTraffic
     def rescue_traffic(*args)
       include CanCanTraffic::Statuses
 
-      rescue_from ActiveRecord::RecordNotFound, with: :not_found
-      rescue_from ActiveRecord::RecordInvalid,  with: :unprocessable
-      rescue_from CanCan::AccessDenied,         with: :access_denied
+      def self.log_before method
+        Proc.new do |exception|
+          logger.tagged("CancanTraffic") { logger.info "Rescuing from #{ exception.class } with :#{ method }" }
+          send method
+        end
+      end
+
+      rescue_from ActiveRecord::RecordNotFound, with: log_before(:not_found)
+      rescue_from ActiveRecord::RecordInvalid,  with: log_before(:unprocessable)
+      rescue_from CanCan::AccessDenied,         with: log_before(:access_denied)
 
       unless Rails.application.config.consider_all_requests_local
-        rescue_from ActionView::MissingTemplate, with: :not_found
+        rescue_from ActionView::MissingTemplate, with: log_before(:not_found)
       end
     end
 

2336d7d3b48e7e59a1120de7b9dad4ef458303b6
Savater Sebastien <savater.sebastien@gmail.com>
2012-10-12 10:16:30 +0200

    Work without active_record

--- lib/cancan-traffic/controller_additions.rb
+++ lib/cancan-traffic/controller_additions.rb
@@ -27,16 +27,11 @@ module CanCanTraffic
         end
       end
 
-      if defined? ActiveRecord
-        rescue_from ActiveRecord::RecordNotFound, :with => log_before(:not_found)
-        rescue_from ActiveRecord::RecordInvalid,  :with => log_before(:unprocessable)
-      end
+      rescue_from ActiveRecord::RecordNotFound, :with => log_before(:not_found)     if defined? ActiveRecord::RecordNotFound
+      rescue_from ActiveRecord::RecordInvalid,  :with => log_before(:unprocessable) if defined? ActiveRecord::RecordInvalid
+      rescue_from CanCan::AccessDenied,         :with => log_before(:access_denied)
 
-      if defined? Mongoid
-        rescue_from Mongoid::Errors::DocumentNotFound, :with => log_before(:not_found)
-      end
-
-      rescue_from CanCan::AccessDenied, :with => log_before(:access_denied)
+      rescue_from Mongoid::Errors::DocumentNotFound, :with => log_before(:not_found) if defined? Mongoid
 
       unless Rails.application.config.consider_all_requests_local
         rescue_from ActionView::MissingTemplate, :with => log_before(:not_found)
